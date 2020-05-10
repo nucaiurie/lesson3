7edc6842dc9fc201552f5a187889b9d210af5efe
nucaiurie <43534339+nucaiurie@users.noreply.github.com>
2020-05-10 08:48:03 +0300

    multiplication, division, zerodivision error

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('Hi')
+print('Hi, this is lesson 3, give me 2 numbers >= 0')
+print('CARE b will be the number by which you divide')
+#inputtin 2 numbers and doing dirty stuff
+while True:
+    print('Give me number "a"')
+    a=int(input())
+    print('Give me number "b"')
+    b=int(input())
+    if (a*b<0):
+        print('Give me numbers bigger than 0!')
+    else:
+        #operations
+        try:
+            mul=a*b
+            div=a/b
+            print(f'Multiplication={mul}')
+            print(f'Division={div}')
+        except Exception as e:
+            print('You messed up')
+            print('e')
+        except ZeroDivisionError:
+            print('Can not divide by 0!, change "b"')
+        else:
+            print('Thank you!')
+            break
