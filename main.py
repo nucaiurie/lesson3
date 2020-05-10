7fe70b3e97ad5696b3e919219a5d8a6d40d82973
nucaiurie <43534339+nucaiurie@users.noreply.github.com>
2020-05-10 08:54:56 +0300

    edited + -

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
-print('Hi, this is lesson 3')
+print('Hi, this is lesson 3, give me 2 numbers >= 0')
+print('We will be adding and substracting')
 #inputtin 2 numbers and doing dirty stuff
-try:
+while True:
     print('Give me number "a"')
     a=int(input())
     print('Give me number "b"')
     b=int(input())
-    #operations
-    try:
-        summ=a+b
-        diff=a-b
-        print('Summ={}',summ)
-        print('Difference={}', diff)
-    except Exception as e:
-        print('You messed up')
-        print('e')
-
-except ValueError:
-    print('I asked for numbers, man!')
-
-else:
-    print('All good, thanks for playing')
-
-finally:
-    print('Whatevs')
+    if (a*b<0):
+        print('Give me numbers bigger than 0!')
+    else:
+        #operations
+        try:
+            summ=a+b
+            sub=a-b
+            print(f'Summ={summ}')
+            print(f'Substraction={sub}')
+        except Exception as e:
+            print('You messed up')
+            print('e')
+        else:
+            print('Thank you!')
+            break
