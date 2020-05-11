a53910d4be1e14080a67fb5ca4a79ab9e2619ae9
nucaiurie <43534339+nucaiurie@users.noreply.github.com>
2020-05-11 20:45:49 +0300

    lesson 4 - trick, run and check for chance functions + user input for action

--- functions/function_module.py
+++ functions/function_module.py
@@ -1,4 +1,7 @@
 import random
+import wikipedia
+from emoji import emojize
+import datetime
 
 
 def attack():
@@ -7,3 +10,29 @@ def attack():
 
 def heal():
     return random.randint(1, 6)
+
+
+def run():
+    a = random.randint(1, 10)
+    if a < 5:
+        return 'failed'
+    else:
+        return 'success'
+
+
+def trick():
+    a = random.randint(1, 2)
+    if a < 2:
+        return 'failed'
+    else:
+        return 'success'
+
+
+def live_or_die(status, name):
+    if status == 'failed':
+        print(f'At this date, {datetime.datetime.today()}, {name}')
+        print(f'{wikipedia.summary(name, sentences=1)},died')
+    else:
+        print(f'At this date, {datetime.datetime.today()}, {name}')
+        print(f'{wikipedia.summary(name, sentences=1)},lived')
+        print(emojize(':thumbs_up:'))
--- lesson4.py
+++ lesson4.py
@@ -1,14 +1,29 @@
 import wikipedia
-import emoji
-from functions import attack
+from emoji import emojize
+import datetime
+from functions import *
 
 a = {'name': 'Oleg', 'health': 10}
 b = {'name': 'Vologhea', 'health': 10}
 
-print(f'{a.get("name")} attacks {b.get("name")} deals')
+print(f'{a.get("name")} attacks {b.get("name")} ')
 
 b.update({"health": b.get("health")-attack()})
-
-
 print(b)
 
+print(f'{b.get("name")} attacks {a.get("name")} deals')
+b.update({"health": a.get("health")-attack()})
+print(b)
+
+while True:
+    print(f'{a.get("name")} wants to trick or run?')
+    x = input()
+    print(x)
+    if x == 'trick':
+        t = trick()
+        live_or_die(t, a.get("name"))
+        break
+    else:
+        r = run()
+        live_or_die(r, a.get("name"))
+    break
